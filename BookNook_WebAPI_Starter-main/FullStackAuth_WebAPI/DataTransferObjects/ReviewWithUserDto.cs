405ca664be5447ae49fd68a395b28f3b89b1e104
Kira <knguier12@gmail.com>
2023-12-19 18:46:28 -0500

    Review With User Dto Created

---
+++ BookNook_WebAPI_Starter-main/FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class ReviewWithUserDto
+    {
+        public string BookId { get; set; }
+
+        public string Text { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
