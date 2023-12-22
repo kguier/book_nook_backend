b69f00c59a4ae520727a3de69c07e5fedd41e88c
Kira <knguier12@gmail.com>
2023-12-21 20:55:35 -0500

    JSON file exported and added to Project File

---
+++ BookNook_WebAPI_Starter-main/FullStackAuth_WebAPI/Controllers/BookDetailsController.cs
@@ -0,0 +1,42 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using FullStackAuth_WebAPI.DataTransferObjects;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/BookDetails")]
+    [ApiController]
+    public class BookDetailsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookDetailsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<BookDetailsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/BookDetails/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var book = _context.bookId.Find(id);
+            }
+            return StatusCode(200);
+        }
+
+       
+    }
+}
