5a015131e472a31738d3df2ecf34f580ba569c22
Kira <knguier12@gmail.com>
2023-12-21 20:13:18 -0500

    Favorites Controller Created and Written; Not Functional

---
+++ BookNook_WebAPI_Starter-main/FullStackAuth_WebAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,50 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/Favorites")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Favorites/myFavorites
+        [HttpGet("myFavorites"), Authorize]
+        public IActionResult GetUsersFavorites()
+        {
+            try
+            {
+                string userId = User.FindFirstValue("id");
+                var favorites = _context.Favorites.Where(f => f.UserId.Equals(userId));
+                return StatusCode(200, favorites);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
+        }
+
+        
+
+        // POST api/<FavoritesController>
+        [HttpPost, Authorize]
+        public void Post([FromBody] Favorite favorite)
+        {
+            /*return StatusCode(201);*/
+        }
+
+        
+    }
+}
